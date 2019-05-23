2bb6e0ed23515cd26c8b8a4ff1d39a527ca3fbb5
aron <amotis@calpoly.edu>
2019-05-23 11:20:05 -0700

    Commented out C++ bindgen code that doesn't quite work. Will likely remove it soon.

--- build.rs
+++ build.rs
@@ -2,9 +2,10 @@ extern crate bindgen;
 
 use std::env;
 use std::path::PathBuf;
-use bindgen::Builder;
+use bindgen::{builder, Builder};
 
 fn main() {
+    // C libraries.
     for lib in ["bfd", "capstone"].iter() {
         println!("cargo:rustc-link-lib={}", lib);
 
@@ -17,4 +18,25 @@ fn main() {
         bindings.write_to_file(out_path.join(fname))
                 .expect("Couldn't write bindings");
     }
+
+    // C++ libraries.
+    /* FIXME: This doesn't work and might not ever...
+    println!("cargo:rustc-link-lib=dyninstAPI");
+    println!("cargo:rustc-link-lib=common");
+    println!("cargo:rustc-link-lib=boost_system");
+
+    let header = "dyninst_wrapper.h";
+    let bindings = builder().header(header)
+                            .clang_arg("-x c++")
+                            .clang_arg("-lstdc++")
+                            .clang_arg("-std=c++14")
+                            //.clang_arg("")
+                            .generate()
+                            .expect("Unable to open dyninst bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let fname = "bgen_dyninst.rs";
+
+    bindings.write_to_file(out_path.join(fname))
+            .expect("Couldn't write dyninst bindings");
+    */
 }
