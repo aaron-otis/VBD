d6b9e454c5b87cd3eb98493346bc8ce71acb5aa3
aron <amotis@calpoly.edu>
2019-06-01 23:43:33 -0700

    Lots of changes due to the fact nothing would work unless a large amount of changes where made.
    Added struct Edge to represent edges. Currently contains an entry and
    exit, but will be expanded in the future when edge types are needed.
    
    Changed the typed of 'edges' in CFG to use the new Edge class.
    Changed the 'new' function to include a copy of the text segment in
    order to ensure only valid addresses are searched.
    Added three new methods: 'get_successors', 'get_predecessors', and
    'get_block'. These assist in CFG creation, but can be used at any time.
    
    The simple iteration over basic blocks is now used to create a temporary
    edge set in order to build a more complete set. Afterwards, we check for
    calls and create a vector to use as a stack of return addresses. Using
    (the probably wrongly named) 'dfs' helper function, we traverse the
    graph until a return is found, recursively traversing addition calls
    found, until a return is found and an edge can be added from the block
    containing the return instruction to the address on the return stack.
    
    This will potentially create many edges into and out of functions. Thus,
    in order to process this new graph, a return stack similar what was used
    to create this graph must be used when processing it.

--- src/graphs.rs
+++ src/graphs.rs
@@ -1,29 +1,87 @@
+use std::fmt;
 use std::collections::HashSet;
-use binary::binary::BasicBlock;
+use binary::binary::{Binary, BasicBlock};
+use binary::section::Section;
 use capstone;
 
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub struct Edge {
+    pub entry: u64,
+    pub exit: u64,
+}
+
+impl Edge {
+    pub fn new(entry: u64, exit: u64) -> Edge {
+        Edge {entry: entry, exit: exit}
+    }
+}
+
+impl fmt::Display for Edge {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "(0x{:x}, 0x{:x})", self.entry, self.exit)
+    }
+}
+
+#[derive(Clone)]
 pub struct CFG<'a> {
     pub start: u64,
     pub end: u64,
-    pub edges: HashSet<(u64, u64)>,
+    pub edges: HashSet<Edge>,
     pub vertices: &'a Vec<BasicBlock>
 }
 
 impl CFG<'_> {
-    pub fn new(blocks: &Vec<BasicBlock>) -> CFG {
-        let last_block = blocks.len() - 1;
-        let last_insn = blocks[last_block].instructions.len() - 1;
-        let edges = CFG::detect_edges(blocks.as_slice());
+    pub fn new(bin: &Binary) -> Option<CFG> {
+        let last_block = bin.blocks.len() - 1;
+        let last_insn = bin.blocks[last_block].instructions.len() - 1;
+        let text = match bin.clone().get_text_section() {
+            Ok(sec) => sec,
+            Err(_e) => return None,
+        };
+        let edges = CFG::detect_edges(bin.blocks.as_slice(), &text);
 
-        CFG {start: blocks[0].entry,
-             end: blocks[last_block].instructions[last_insn].address,
-             edges: edges,
-             vertices: blocks
-            }
+        Some(CFG {start: bin.blocks[0].entry,
+                  end: bin.blocks[last_block].instructions[last_insn].address,
+                  edges: edges,
+                  vertices: &bin.blocks
+                  })
     }
 
-    fn detect_edges(blocks: &[BasicBlock]) -> HashSet<(u64, u64)> {
-        let mut edges: HashSet<(u64, u64)> = HashSet::new();
+    pub fn get_successors(&self, addr: u64) -> HashSet<Edge> {
+        let mut successors: HashSet<Edge> = HashSet::new();
+
+        for edge in &self.edges {
+            if edge.entry == addr {
+                successors.insert(edge.clone());
+            }
+        }
+
+        successors
+    }
+
+    pub fn get_predecessors(&self, addr: u64) -> HashSet<Edge> {
+        let mut predecessors: HashSet<Edge> = HashSet::new();
+
+        for edge in &self.edges {
+            if edge.exit == addr {
+                predecessors.insert(edge.clone());
+            }
+        }
+
+        predecessors
+    }
+
+    pub fn get_block(&self, addr: u64) -> Option<&BasicBlock> {
+        for block in self.vertices {
+            if block.entry == addr {
+                return Some(&block);
+            }
+        }
+        None
+    }
+
+    fn detect_edges(blocks: &[BasicBlock], text: &Section) -> HashSet<Edge> {
+        let mut edges: HashSet<Edge> = HashSet::new();
 
         /* Create a partial edge set.
          * Determine edges by iterating through each block to find the addresses
@@ -42,7 +100,7 @@ impl CFG<'_> {
 
                 // We currently can only handle immediate targets and fail on all others.
                 match target {
-                    Some(addr) => edges.insert((block.entry, addr)),
+                    Some(addr) => edges.insert(Edge::new(block.entry, addr)),
                     None => true,
                 };
 
@@ -52,7 +110,7 @@ impl CFG<'_> {
                  */
                 if !last_insn.is_unconditional_cflow_ins() {
                     if CFG::block_exists(blocks, next_insn) {
-                        edges.insert((block.entry, next_insn));
+                        edges.insert(Edge::new(block.entry, next_insn));
                     }
                 }
 
@@ -61,10 +119,74 @@ impl CFG<'_> {
              * to the next sequential block.
              */
             else {
-                edges.insert((block.entry, next_insn));
+                edges.insert(Edge::new(block.entry, next_insn));
             }
         }
 
+        // Create a temporary CFG.
+        let last_block = blocks.len() - 1;
+        let last_insn = blocks[last_block].instructions.len() - 1;
+        let cfg = CFG {start: blocks[0].entry,
+                       end: blocks[last_block].instructions[last_insn].address,
+                       edges: edges.clone(),
+                       vertices: &blocks.to_vec().clone()};
+
+        /* Iterate over each block and use partial edge set to resolve edges due to
+         * returns.
+         */
+        for block in blocks {
+            let last_insn = &block.instructions[block.instructions.len() - 1];
+            let next_insn = last_insn.address + last_insn.size as u64;
+            let mut seen: HashSet<u64> = HashSet::new();
+
+            // Only process blocks that call functions.
+            if last_insn.id == capstone::x86_insn_X86_INS_CALL &&
+               !seen.contains(&block.entry) {
+                let target = last_insn.get_immediate_target();
+
+                match target {
+                    Some(addr) => {
+                        // Only search for returns if there is a valid return address.
+                        if CFG::block_exists(blocks, next_insn) {
+
+                            /* There are two scenarios: The target address is either
+                             * inside the .text segment or not. We handle these
+                             * differently.
+                             */
+                            if text.contains(addr) {
+                                /* The target call is in the text section and we can
+                                 * traverse its subgraph.
+                                 */
+                                let mut stack: Vec<u64> = Vec::new();
+                                stack.push(next_insn);
+
+                                for edge in CFG::dfs(addr, &cfg, &mut seen, stack) {
+                                    edges.insert(edge);
+                                }
+                            }
+                            else {
+                                /* The target address is outside of the .text segment,
+                                 * we will ignore the fact that a call occurred and
+                                 * just place an edge from the calling block to the
+                                 * block the function would have returned to.
+                                 */
+                                edges.remove(&Edge::new(block.entry, addr));
+                                edges.insert(Edge::new(block.entry, next_insn));
+                                println!("Replaced (0x{:x}, 0x{:x}) with (0x{:x}, 0x{:x})",
+                                         block.entry, addr, block.entry, next_insn);
+                            }
+                        }
+
+                    },
+                    // If 'target' cannot be resolved, add a pseudo fall through edge.
+                    None => if CFG::block_exists(blocks, next_insn) {
+                        edges.insert(Edge::new(block.entry, next_insn));
+                    },
+                };
+            }
+        }
+
+        println!("# of edges: {}", edges.len());
         edges
     }
 
@@ -75,5 +197,57 @@ impl CFG<'_> {
             }
         }
         false
+    }
+
+    fn dfs(addr: u64, graph: &CFG, seen: &mut HashSet<u64>,
+              mut ret_stack: Vec<u64>) -> HashSet<Edge> {
+        let mut new_edges: HashSet<Edge> = HashSet::new();
+
+        // Try to get the block with entry 'addr'.
+        let block = match graph.get_block(addr) {
+            Some(block) => block,
+            None => return new_edges,
+        };
+
+        // Base case.
+        if block.returns() {
+            // Ensure the stack has a value.
+            match ret_stack.pop() {
+                Some(ret_addr) => {
+                    println!("Found 0x{:x} on the stack", ret_addr);
+                    new_edges.insert(Edge::new(block.entry, ret_addr));
+                },
+                None => (),
+            };
+            return new_edges;
+        }
+
+        // Recursively search this block's successors.
+        for edge in graph.get_successors(block.entry) {
+            // Prevent infinite loops by checking if this block has been seen.
+            if !seen.contains(&edge.exit) {
+                seen.insert(edge.exit);
+
+                // If the next block calls a function, push return address on the stack.
+                match graph.get_block(edge.exit) {
+                    Some(b) => if b.has_call() {
+                        match b.instructions.last() {
+                            Some(insn) => match insn.get_immediate_target() {
+                                Some(target) => ret_stack.push(target),
+                                None => (),
+                            },
+                            None => (),
+                        };
+                    },
+                    None => (),
+                };
+
+                for new_edge in CFG::dfs(edge.exit, graph, seen, ret_stack.clone()) {
+                    new_edges.insert(new_edge);
+                }
+            }
+        }
+
+        new_edges
     }
 }
