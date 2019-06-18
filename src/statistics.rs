0e9b043e2ce59ab556d1d3120f808cd29aa0e09e
aron <amotis@calpoly.edu>
2019-06-18 11:38:45 -0700

    Updated to use Instruction instead of cs_insn.

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,20 +1,13 @@
-use std::collections::{HashMap};
-use std::ffi::{CStr};
-use binary::binary::Binary;
-
-include!(concat!(env!("OUT_DIR"), "/bgen_capstone.rs"));
+use std::collections::HashMap;
+use binary::binary::{Binary, Instruction};
+use capstone;
 
 pub fn count_instructions(bin: &Binary) -> HashMap<String, u64> {
     let mut counts: HashMap<String, u64> = HashMap::new();
 
-    for block in &bin.instructions {
-        for insn in block {
-            unsafe {
-                let mnemonic = CStr::from_ptr((*insn).mnemonic.as_ptr()).to_string_lossy();
-                let counter = counts.entry(mnemonic.to_string()).or_insert(0);
-                *counter += 1;
-            }
-        }
+    for insn in bin.instructions() {
+        let counter = counts.entry(insn.mnemonic).or_insert(0);
+        *counter += 1;
     }
 
     counts
@@ -26,35 +19,33 @@ pub fn print_statistics(counts: &HashMap<String, u64>) {
     }
 }
 
-fn is_bitop(insn: cs_insn) -> bool {
-    unsafe {
-        if insn.id == x86_insn_X86_INS_AND ||
-           insn.id == x86_insn_X86_INS_ANDPS ||
-           insn.id == x86_insn_X86_INS_ANDNPS ||
-           insn.id == x86_insn_X86_INS_ANDN ||
-           insn.id == x86_insn_X86_INS_ANDPD ||
-           insn.id == x86_insn_X86_INS_ANDNPD ||
-           insn.id == x86_insn_X86_INS_PAND ||
-           insn.id == x86_insn_X86_INS_PANDN ||
-           insn.id == x86_insn_X86_INS_OR ||
-           insn.id == x86_insn_X86_INS_ORPS ||
-           insn.id == x86_insn_X86_INS_ORPD ||
-           insn.id == x86_insn_X86_INS_POR ||
-           insn.id == x86_insn_X86_INS_XOR ||
-           insn.id == x86_insn_X86_INS_XORPS ||
-           insn.id == x86_insn_X86_INS_XORPD ||
-           insn.id == x86_insn_X86_INS_PXOR 
-           /*
-           insn.id == x86_insn_X86_INS_ ||
-           insn.id == x86_insn_X86_INS_ ||
-           insn.id == x86_insn_X86_INS_ ||
-           insn.id == x86_insn_X86_INS_ ||
-           insn.id == x86_insn_X86_INS_ ||
-           insn.id == x86_insn_X86_INS_ ||
-           */
-        {
-            return true
-        }
+pub fn is_bitop(insn: &Instruction) -> bool {
+    if insn.id == capstone::x86_insn_X86_INS_AND ||
+       insn.id == capstone::x86_insn_X86_INS_ANDPS ||
+       insn.id == capstone::x86_insn_X86_INS_ANDNPS ||
+       insn.id == capstone::x86_insn_X86_INS_ANDN ||
+       insn.id == capstone::x86_insn_X86_INS_ANDPD ||
+       insn.id == capstone::x86_insn_X86_INS_ANDNPD ||
+       insn.id == capstone::x86_insn_X86_INS_PAND ||
+       insn.id == capstone::x86_insn_X86_INS_PANDN ||
+       insn.id == capstone::x86_insn_X86_INS_OR ||
+       insn.id == capstone::x86_insn_X86_INS_ORPS ||
+       insn.id == capstone::x86_insn_X86_INS_ORPD ||
+       insn.id == capstone::x86_insn_X86_INS_POR ||
+       insn.id == capstone::x86_insn_X86_INS_XOR ||
+       insn.id == capstone::x86_insn_X86_INS_XORPS ||
+       insn.id == capstone::x86_insn_X86_INS_XORPD ||
+       insn.id == capstone::x86_insn_X86_INS_PXOR 
+       /*
+       insn.id == x86_insn_X86_INS_ ||
+       insn.id == x86_insn_X86_INS_ ||
+       insn.id == x86_insn_X86_INS_ ||
+       insn.id == x86_insn_X86_INS_ ||
+       insn.id == x86_insn_X86_INS_ ||
+       insn.id == x86_insn_X86_INS_ ||
+       */
+    {
+        return true
     }
 
     false
